f5425f9ee23551216fdd80a35b1fdc240980f83f
Luis Navarro <navarrojr89@gmail.com>
2021-02-14 12:02:34 +0100

    Includes some comments

--- src/main/java/com/lamn/microservices/eurekaserver/EurekaServierApplication.java
+++ src/main/java/com/lamn/microservices/eurekaserver/EurekaServierApplication.java
@@ -4,10 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * The type Eureka servier application.
+ */
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServierApplication {
 
+	/**
+	 * The entry point of application.
+	 *
+	 * @param args the input arguments
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServierApplication.class, args);
 	}
